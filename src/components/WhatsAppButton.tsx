290143e8fabcd2fe5c6721bb1c8f9a234bdb7f07
[dyad] <reboucasfee@gmail.com>
2025-09-14 15:31:00 -0300

    [dyad] Trocado o ícone do WhatsApp por um novo estilo - wrote 1 file(s)

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
-import { IoLogoWhatsapp } from "react-icons/io"; // Importando um ícone diferente
+import { MdWhatsapp } from "react-icons/md"; // Importando um ícone diferente
 
 const WhatsAppButton = () => {
   // IMPORTANTE: Substitua este número pelo seu número de WhatsApp com o código do país.
@@ -22,7 +22,7 @@ const WhatsAppButton = () => {
             size="icon"
             className="bg-green-500 hover:bg-green-600 text-white rounded-full h-14 w-14 shadow-lg flex items-center justify-center"
           >
-            <IoLogoWhatsapp className="h-7 w-7" /> {/* Usando o novo ícone */}
+            <MdWhatsapp className="h-7 w-7" /> {/* Usando o novo ícone */}
           </Button>
         </a>
       </TooltipTrigger>
